931484bb6374964270c2d26d58d466269fd34187
jiffingwalter <81245937+jiffingwalter@users.noreply.github.com>
2023-09-30 16:37:13 -0400

    Made easy access panel for admin pages, fixed missing contacts index

---
+++ admin/index.php
@@ -0,0 +1,7 @@
+Temp page for easy testing on admin pages <hr>
+
+<a href="./awards/index.php">Awards management</a></br>
+<a href="./contacts/index.php">Contacts management</a></br>
+<a href="./pages/index.php">Pages management</a></br>
+<a href="./products/index.php">Products management</a></br>
+<a href="./team/index.php">Team management</a></br>
