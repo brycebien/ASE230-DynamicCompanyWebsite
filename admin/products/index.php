2728c3f074f82d7760adaff5e223394f7375f9d0
brycebien <brycebien@gmail.com>
2023-10-02 09:52:20 -0400

    added a create_product function and a get_products function

---
+++ admin/products/index.php
@@ -0,0 +1,11 @@
+<head>
+    <title>Products</title>
+</head>
+
+<body>
+    <h1>Products</h1>
+    <table>
+        <!-- table listing all available products -->
+        
+    </table>
+</body>
---
+++ admin/products/products.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../../lib/readJSON.php');
+$products_list=get_products(); // used for retrieving and indexing all items
+
+//retrieve and index
+function get_products(){
+    return readJSON('../../data/products.json');
+}
+
+function create_product($entry_in){
+    //echo 'Creating entry '.$entry_in['id'].'...';
+    $entries_updated=get_products(); // set enteries_updated to the json data as php array
+    $entries_updated[count($entries_updated)]=$entry_in; // add the new entry to the json data
+    $updated = json_encode($entries_updated,JSON_PRETTY_PRINT); // set updated to the json data as json
+    echo $updated;
+    file_put_contents('../../data/products.json',$updated); // update the json data
+
+}
+?>
