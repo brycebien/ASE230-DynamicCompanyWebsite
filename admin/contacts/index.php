dda61eda1090e2014dc81557a85a3c60876c0195
brycebien <brycebien@gmail.com>
2023-10-02 09:07:35 -0400

    added boilerplate for contact folder

---
+++ admin/contacts/index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<head>
+    <title>Contact Requests</title>
+</head>
+
+<body>
+    <h1>Contact Requests</h1>
+    <table>
+        <!-- list the available contact requests -->
+    </table>
+</body>
