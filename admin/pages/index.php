fc710fdfcf16bc98f89094330191a14c8eb98362
brycebien <brycebien@gmail.com>
2023-10-03 09:47:55 -0400

    created boilerplate for pages -> index.php

---
+++ admin/pages/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once('pages.php');
+?>
+
+<head>
+    <title>Manage Pages</title>
+</head>
+
+<body>
+    <h1>Manage Pages</h1>
+
+    <table border="1" cellpadding="5" cellspacing="2" style="min-width:100px">
+        <td><a href="create.php">Create a new page</a></td>
+    </table>
+    <table border="1" cellpadding="5" cellspacing="2" style="min-width:100px">
+
+    </table>
+</body>
