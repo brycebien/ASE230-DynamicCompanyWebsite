d7ff8b4f5c5c29f69cf8ad359f00363dd45c64e2
brycebien <brycebien@gmail.com>
2023-10-03 10:20:09 -0400

    added create_page() and create page form

--- admin/pages/pages.php
+++ admin/pages/pages.php
@@ -1,12 +1,24 @@
 <?php
 require_once('../../theme/functions.php');
+
+function get_pages(){
+
+}
 #creating a new page
 function create_page($page){
-    if(!file_exists('../../'.$page['title'].'.html')){
-
+    if(!file_exists('../../'.$page['name'].'.html')){
+        file_put_contents('../../pages/'.$page['name'].'.html',$page['code']);
+        echo '<h1>Page has been created at ../../pages/'.$page['name'].'.html</h1>';
+    ?>
+        <a href="../../pages/<?=$page['name']?>.html">Go to <?=$page['name']?>.html</a>
+    <?php
     }else{
         echo 'page already exists';
+    ?>
+        <a href='index.php'>go back to page manager</a>
+    <?php
         die();
     }
 }
+
 ?>
