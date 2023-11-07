02fc519b2c2035ec028d32224dd63f947840f19b
brycebien <brycebien@gmail.com>
2023-11-06 19:29:26 -0500

    completed create award using OOP

--- admin/awards/awards.php
+++ admin/awards/awards.php
@@ -17,27 +17,27 @@ require_once('../../lib/readCSV.php');
 //     die;
 // }
 
-function edit_award($entry_in){
-    echo 'Saving changes to entry '.$entry_in['id'].' ...';
-    $entries_existing=get_awards();
-    $entries_updated=fopen('../../data/awards.csv','w');
+// function edit_award($entry_in){
+//     echo 'Saving changes to entry '.$entry_in['id'].' ...';
+//     $entries_existing=get_awards();
+//     $entries_updated=fopen('../../data/awards.csv','w');
 
-    for ($row=0;$row < count($entries_existing);$row++){
-        var_dump($entries_existing[$row]);
-        if ($entries_existing[$row]['id'] == $entry_in['index']){
-            $entries_existing[$row]['year']=$entry_in['year'];
-            $entries_existing[$row]['award']=$entry_in['award'];
-        }
-        echo '<br>';
-    }
-    fputcsv($entries_updated,['year','award','id'],';');
-    foreach ($entries_existing as $fields){
-        fwrite($entries_updated,implode(';',$fields)."\n");
-    }
-    fclose($entries_updated);
-    header('Location: detail.php?index='.$entry_in['index']); //redirect back to entry
-    die;
-}
+//     for ($row=0;$row < count($entries_existing);$row++){
+//         var_dump($entries_existing[$row]);
+//         if ($entries_existing[$row]['id'] == $entry_in['index']){
+//             $entries_existing[$row]['year']=$entry_in['year'];
+//             $entries_existing[$row]['award']=$entry_in['award'];
+//         }
+//         echo '<br>';
+//     }
+//     fputcsv($entries_updated,['year','award','id'],';');
+//     foreach ($entries_existing as $fields){
+//         fwrite($entries_updated,implode(';',$fields)."\n");
+//     }
+//     fclose($entries_updated);
+//     header('Location: detail.php?index='.$entry_in['index']); //redirect back to entry
+//     die;
+// }
 
 // function delete_award($entry_in){
 //     echo 'Deleting entry '.$entry_in['id'].'...';
@@ -114,7 +114,7 @@ class AwardsManager{
             }
         }
         if(!$isInCSV){
-            fwrite($new_entries,implode(';',['year'=>$award->getYear(),'award'=>$award->getTitle(), 'id'=>count($entries)-1])."\n");
+            fwrite($new_entries,implode(';',['year'=>$award->getYear(),'award'=>$award->getTitle(), 'id'=>count($entries)])."\n");
         }
         fclose($new_entries);
     }
